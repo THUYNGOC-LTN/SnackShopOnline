c8b4d0efa6b57e8a3bc795a95d10fa0b27a981f7
THUYNGOC-LTN <lamngoc21082005@gmail.com>
2026-05-16 22:37:59 +0700

    fix clean fixturessasasa

--- snackshop/settings.py
+++ snackshop/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
 
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'axes.middleware.AxesMiddleware',
+    # 'axes.middleware.AxesMiddleware',
 ]
 
 ROOT_URLCONF = 'snackshop.urls'
