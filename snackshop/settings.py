84e5762fdb146d342c428ffe3bb3ca44c4268189
THUYNGOC-LTN <lamngoc21082005@gmail.com>
2026-05-16 18:08:44 +0700

    fix: deploy test

--- snackshop/settings.py
+++ snackshop/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'snackshop.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+        default=os.getenv("DATABASE_URL")
     )
 }
 
