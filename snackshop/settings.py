4163e9b323cadb5621be9b324dd81b31e8651c9f
THUYNGOC-LTN <lamngoc21082005@gmail.com>
2026-05-17 15:10:12 +0700

    asdasdasasdasd

--- snackshop/settings.py
+++ snackshop/settings.py
@@ -157,6 +157,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 # ========================
 # LOGIN / LOGOUT
 # ========================
