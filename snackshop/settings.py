ed835935b7e31208a28f79f70d0bfce23493aeec
THUYNGOC-LTN <lamngoc21082005@gmail.com>
2026-05-17 14:51:17 +0700

    asdasd

--- snackshop/settings.py
+++ snackshop/settings.py
@@ -144,9 +144,9 @@ WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ['.map']
 # ========================
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": config("CLOUDINARY_API_KEY"),
-    "API_SECRET": config("CLOUDINARY_API_SECRET"),
+    "cloud_name": config("CLOUDINARY_CLOUD_NAME"),
+    "api_key": config("CLOUDINARY_API_KEY"),
+    "api_secret": config("CLOUDINARY_API_SECRET"),
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
