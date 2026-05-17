cffbb58b5050bcbd552d72f7fbe40e3bfe9d7830
THUYNGOC-LTN <lamngoc21082005@gmail.com>
2026-05-17 12:36:53 +0700

    fix .map

--- snackshop/settings.py
+++ snackshop/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_USE_FINDERS = True
 WHITENOISE_MANIFEST_STRICT = False
 WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ['.map']
