d0eed87d15c43f096d7cc8238ba042d6b18bb6c9
THUYNGOC-LTN <lamngoc21082005@gmail.com>
2026-05-17 14:31:31 +0700

    fix logic paymentaask

--- snackshop/settings.py
+++ snackshop/settings.py
@@ -144,19 +144,12 @@ WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ['.map']
 # ========================
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': config('CLOUDINARY_API_KEY'),
-    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": config("CLOUDINARY_API_KEY"),
+    "API_SECRET": config("CLOUDINARY_API_SECRET"),
 }
 
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 # ========================
 # LOGIN / LOGOUT
 # ========================
